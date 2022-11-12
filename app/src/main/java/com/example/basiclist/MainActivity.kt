20c8abcaadeb616de778deafaed2cddf57559015
ChenWarren <72281162+ChenWarren@users.noreply.github.com>
2022-11-11 21:45:28 -0800

    added greeting screen

--- app/src/main/java/com/example/basiclist/MainActivity.kt
+++ app/src/main/java/com/example/basiclist/MainActivity.kt
@@ -3,14 +3,10 @@ package com.example.basiclist
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -31,6 +27,21 @@ class MainActivity : ComponentActivity() {
 @Composable
 private fun MyApp(
     modifier: Modifier = Modifier,
+) {
+    var shouldShowOnboarding by remember { mutableStateOf(true)}
+
+    Surface(modifier) {
+        if(shouldShowOnboarding) {
+            OnboardingScreen(onContinueClicked = { shouldShowOnboarding = false })
+        } else {
+            Greeting()
+        }
+    }
+}
+
+@Composable
+private fun Greeting(
+    modifier: Modifier = Modifier,
     names: List<String> = listOf("World", "Android")
 ) {
     Surface(
@@ -48,17 +59,61 @@ private fun MyApp(
 }
 
 @Composable
+fun OnboardingScreen(
+    modifier: Modifier = Modifier,
+    onContinueClicked: () -> Unit,
+) {
+
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Welcome to the Basic List")
+        Button(
+            onClick = onContinueClicked,
+            modifier = Modifier.padding(vertical = 24.dp)
+        ) {
+            Text(text = "Continue")
+        }
+    }
+}
+
+@Composable
 private fun Greeting(name: String) {
+    val expanded = remember { mutableStateOf(false) }
+    val extraPadding = if (expanded.value) 48.dp else 0.dp
     Surface(
         color = MaterialTheme.colorScheme.primary,
         modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
     ) {
-        Column(
-            modifier = Modifier.padding(24.dp).fillMaxWidth()
+        Row(
+            horizontalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier
+                .padding(24.dp)
+                .fillMaxWidth()
+                .padding(bottom = extraPadding)
         ) {
-            Text(text = "Hello,")
-            Text(text = "$name!")
+            Column(
+                modifier = Modifier.weight(1f)
+            ) {
+                Text(text = "Hello,")
+                Text(text = "$name!")
+            }
+            ElevatedButton(
+                onClick = { expanded.value = !expanded.value }
+            ) {
+                Text(if (expanded.value) "Show less" else "Show more")
+            }
         }
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Composable
+fun OnboardingPreview() {
+    BasicListTheme {
+        OnboardingScreen(onContinueClicked = {})
     }
 }
 
@@ -66,6 +121,6 @@ private fun Greeting(name: String) {
 @Composable
 fun DefaultPreview() {
     BasicListTheme {
-        MyApp()
+        MyApp(Modifier.fillMaxSize())
     }
 }
