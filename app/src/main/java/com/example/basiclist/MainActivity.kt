a14f01a36c59406af3b63a0d8fb28afcd9565130
ChenWarren <72281162+ChenWarren@users.noreply.github.com>
2022-11-13 13:09:23 -0800

    added styles and icon button

--- app/src/main/java/com/example/basiclist/MainActivity.kt
+++ app/src/main/java/com/example/basiclist/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.basiclist
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -9,11 +10,16 @@ import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.basiclist.ui.theme.BasicListTheme
@@ -34,10 +40,10 @@ class MainActivity : ComponentActivity() {
 private fun MyApp(
     modifier: Modifier = Modifier,
 ) {
-    var shouldShowOnboarding by rememberSaveable { mutableStateOf(true)}
+    var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
 
     Surface(modifier) {
-        if(shouldShowOnboarding) {
+        if (shouldShowOnboarding) {
             OnboardingScreen(onContinueClicked = { shouldShowOnboarding = false })
         } else {
             Greeting()
@@ -48,7 +54,7 @@ private fun MyApp(
 @Composable
 private fun Greeting(
     modifier: Modifier = Modifier,
-    names: List<String> = List(100) {"$it"}
+    names: List<String> = List(100) { "$it" }
 ) {
     Surface(
         modifier = modifier,
@@ -89,7 +95,7 @@ fun OnboardingScreen(
 private fun Greeting(name: String) {
     var expanded by rememberSaveable { mutableStateOf(false) }
     val extraPadding by animateDpAsState(
-        targetValue = if(expanded) 48.dp else 0.dp,
+        targetValue = if (expanded) 48.dp else 0.dp,
         animationSpec = spring(
             dampingRatio = Spring.DampingRatioMediumBouncy,
             stiffness = Spring.StiffnessLow
@@ -107,20 +113,43 @@ private fun Greeting(name: String) {
                 .padding(bottom = extraPadding)
         ) {
             Column(
-                modifier = Modifier.weight(1f)
+                modifier = Modifier
+                    .weight(1f)
                     .padding(bottom = extraPadding.coerceAtLeast(0.dp))
             ) {
                 Text(text = "Hello,")
-                Text(text = "$name")
+                Text(
+                    text = "$name",
+                    style = MaterialTheme.typography.headlineMedium.copy(
+                        fontWeight = FontWeight.ExtraBold
+                    )
+                )
+                if (expanded) {
+                    Text(text = ("Composem ipsum color sit lazy," + "padding theme elit, sed do bouncy."))
+                }
             }
-            ElevatedButton(
+            IconButton(
                 onClick = { expanded = !expanded }
             ) {
-                Text(if (expanded) "Show less" else "Show more")
+                Icon(
+                    imageVector = if (expanded) Icons.Filled.ArrowBack else Icons.Filled.ArrowDropDown,
+                    contentDescription = if (expanded) {
+                        stringResource(id = R.string.show_less)
+                    } else {
+                        stringResource(id = R.string.show_more)
+                    }
+                )
             }
         }
     }
 }
+
+@Preview(
+    showBackground = true,
+    widthDp = 320,
+    uiMode = UI_MODE_NIGHT_YES,
+    name = "Dark"
+)
 
 @Preview(showBackground = true, widthDp = 320, heightDp = 320)
 @Composable
@@ -130,10 +159,16 @@ fun OnboardingPreview() {
     }
 }
 
+@Preview(
+    showBackground = true,
+    widthDp = 320,
+    uiMode = UI_MODE_NIGHT_YES,
+    name = "Dark"
+)
 @Preview(showBackground = true, widthDp = 320)
 @Composable
 fun DefaultPreview() {
     BasicListTheme {
-        MyApp(Modifier.fillMaxSize())
+        Greeting()
     }
 }
